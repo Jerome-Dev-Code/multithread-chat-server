2bbbe6f14f05b1ec1fa076a5283a1fe628d14376
DjeDev <jeromecournil@gmail.com>
2026-01-20 20:33:44 +0400

    correct integration test to take VM latency into account during execution

--- src/test/java/com/portfolio/chat/ChatSystemIntegrationIT.java
+++ src/test/java/com/portfolio/chat/ChatSystemIntegrationIT.java
@@ -43,12 +43,8 @@ class ChatSystemIntegrationIT {
         t.setDaemon(true);
         t.start();
 
-        // On laisse un court instant pour le démarrage
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        // Pause plus longue pour la CI
+        try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
     }
 
     @AfterAll
